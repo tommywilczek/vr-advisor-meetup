030dd0db1ce75b1a83dadc92fa758c96e97e9fac
Tommy Wilczk <tommywilczek@utexas.edu>
2020-03-04 17:41:46 -0600

    Adding project

---
+++ Assets/Photon/PhotonVoice/Demos/DemoVoicePun/Scripts/RotationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform cameraTransform;
+
+    void Start()
+    {
+        playerTransform = this.transform;
+        cameraTransform = GameObject.Find("Main Camera").GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform.rotation = cameraTransform.rotation;
+        //Debug.Log(cameraTransform.rotation);
+    }
+}
